fdd1b4dc424177b0a5f50bf512eb8903ba256602
Shadyaobuya <shadyaobuyagard@gmail.com>
2021-07-02 18:43:20 +0300

    Second Commit

--- tasks/views.py
+++ tasks/views.py
@@ -8,8 +8,9 @@ from django.urls import reverse
 list_of_tasks=[]
 
 class NewTask(forms.Form):
-    task=forms.CharField(label="New Task")
-    priority=forms.IntegerField(label="Priority",max_value=10,min_value=1)
+    task=forms.CharField(widget=forms.TextInput(attrs={"class":"form-control form-control",'style': 'width: 300px;'}))
+    priority=forms.IntegerField(label="Priority",max_value=10,min_value=1,widget=forms.NumberInput(attrs={"class":"form-control form-control",'style': 'width: 300px;'}))
+    duration=forms.IntegerField(label="Duration in Minutes",min_value=30,widget=forms.NumberInput(attrs={"class":" form-control","min":"30",'style': 'width: 300px;'}))
 
 
 def index(request):
@@ -22,8 +23,11 @@ def addTask(request):
         form_data=NewTask(request.POST)
         if form_data.is_valid():
             task_entered=form_data.cleaned_data["task"]
-            # priority_entered=form_data.cleaned_data["priority"]
-            list_of_tasks.append(task_entered)
+            duration=form_data.cleaned_data["duration"]
+                
+            list_of_tasks.append(f"{task_entered}..............{duration} Mins")
+
+            # list_of_tasks.append(data)
             return HttpResponseRedirect(reverse("index"))
         else:
             return render(request,"tasks/addtask.html",{"form":form_data})
